47fff797c8f1176c6c074c9b0da41dc919b14f6b
Phil Browne <philip.browne@megaport.com>
2024-04-04 15:04:08 +0000

    fix: more structs

--- internal/provider/vxc_resource.go
+++ internal/provider/vxc_resource.go
@@ -52,11 +52,122 @@ type vxcResourceModel struct {
 	ContractStartDate types.String `tfsdk:"contract_start_date"`
 	ContractEndDate   types.String `tfsdk:"contract_end_date"`
 
-	// AEndConfiguration  *VXCEndConfiguration `tfsdk:"a_end"`
-	// BEndConfiguration  *VXCEndConfiguration `tfsdk:"b_end"`
+	PortUID types.String `tfsdk:"port_uid"`
 
-	// Resources          *VXCResources        `tfsdk:"resources"`
-	// VXCApproval        *VXCApproval         `tfsdk:"vxc_approval"`
+	AEndConfiguration *vxcEndConfigurationModel `tfsdk:"a_end"`
+	BEndConfiguration *vxcEndConfigurationModel `tfsdk:"b_end"`
+
+	AEndOrderConfiguration *vxcOrderEndPointConfigurationModel `tfsdk:"a_end_order_configuration"`
+	BEndOrderConfiguration *vxcOrderEndPointConfigurationModel `tfsdk:"b_end_order_configuration"`
+
+	Resources   *vxcResourcesModel `tfsdk:"resources"`
+	VXCApproval *vxcApprovalModel  `tfsdk:"vxc_approval"`
+}
+
+// vxcResourcesModel represents the resources associated with a VXC.
+type vxcResourcesModel struct {
+	Interface     []*portInterfaceModel `tfsdk:"interface"`
+	VirtualRouter *virtualRouterModel   `tfsdk:"virtual_router"`
+	VLL           *vllConfigModel       `tfsdk:"vll"`
+	// TODO - CSPConnection struct
+}
+
+// virtualRouterModel maps the virtual router schema data.
+type virtualRouterModel struct {
+	MCRAsn             types.Int64  `tfsdk:"mcr_asn"`
+	ResourceName       types.String `tfsdk:"resource_name"`
+	ResourceType       types.String `tfsdk:"resource_type"`
+	Speed              types.Int64  `tfsdk:"speed"`
+	BGPShutdownDefault types.Bool   `tfsdk:"bgp_shutdown_default"`
+}
+
+// vllConfigModel maps the VLL configuration schema data.
+type vllConfigModel struct {
+	AEndVLAN      types.Int64  `tfsdk:"a_vlan"`
+	BEndVLAN      types.Int64  `tfsdk:"b_vlan"`
+	Description   types.String `tfsdk:"description"`
+	ID            types.Int64  `tfsdk:"id"`
+	Name          types.String `tfsdk:"name"`
+	RateLimitMBPS types.Int64  `tfsdk:"rate_limit_mbps"`
+	ResourceName  types.String `tfsdk:"resource_name"`
+	ResourceType  types.String `tfsdk:"resource_type"`
+}
+
+// vxcApprovalModel maps the approval schema data.
+type vxcApprovalModel struct {
+	Status   types.String `tfsdk:"status"`
+	Message  types.String `tfsdk:"message"`
+	UID      types.String `tfsdk:"uid"`
+	Type     types.String `tfsdk:"type"`
+	NewSpeed types.Int64  `tfsdk:"new_speed"`
+}
+
+// vxcOrderEndPointConfigurationModel maps the end point configuration schema data.
+type vxcOrderEndPointConfigurationModel struct {
+	ProductUID              types.String             `tfsdk:"product_uid,omitempty"`
+	VLAN                    types.Int64              `tfsdk:"vlan,omitempty"`
+	PartnerConfig           *vxcPartnerConfiguration `tfsdk:"partner_config,omitempty"`
+	*vxcOrderMVEConfigModel `tfsdk:"mve_config,omitempty"`
+}
+
+// vxcOrderMVEConfigModel maps the MVE configuration schema data.
+type vxcOrderMVEConfigModel struct {
+	InnerVLAN             types.Int64 `tfsdk:"inner_vlan,omitempty"`
+	NetworkInterfaceIndex types.Int64 `tfsdk:"vnic_index"`
+}
+
+// vxcPartnerConfiguration is an interface to ensure the partner configuration is implemented.
+type vxcPartnerConfiguration interface {
+	IsParnerConfiguration()
+}
+
+// vxcPartnerConfigAWSModel maps the partner configuration schema data for AWS.
+type vxcPartnerConfigAWSModel struct {
+	vxcPartnerConfiguration
+	ConnectType       types.String `tfsdk:"connect_type"`
+	Type              types.String `tfsdk:"type"`
+	OwnerAccount      types.String `tfsdk:"owner_account"`
+	ASN               types.Int64  `tfsdk:"asn,omitempty"`
+	AmazonASN         types.Int64  `tfsdk:"amazon_asn,omitempty"`
+	AuthKey           types.String `tfsdk:"auth_key,omitempty"`
+	Prefixes          types.String `tfsdk:"prefixes,omitempty"`
+	CustomerIPAddress types.String `tfsdk:"customer_ip_address,omitempty"`
+	AmazonIPAddress   types.String `tfsdk:"amazon_ip_address,omitempty"`
+	ConnectionName    types.String `tfsdk:"name,omitempty"`
+}
+
+// vxcPartnerConfigAzureModel maps the partner configuration schema data for Azure.
+type vxcPartnerConfigAzureModel struct {
+	vxcPartnerConfiguration
+	ConnectType types.String `tfsdk:"connect_type"`
+	ServiceKey  types.String `tfsdk:"service_key"`
+}
+
+// vxcPartnerConfigGoogleModel maps the partner configuration schema data for Google.
+type vxcPartnerConfigGoogleModel struct {
+	vxcPartnerConfiguration
+	ConnectType types.String `tfsdk:"connect_type"`
+	PairingKey  types.String `tfsdk:"pairing_key"`
+}
+
+// vxcPartnerConfigOracleModel maps the partner configuration schema data for Oracle.
+type vxcPartnerConfigOracleModel struct {
+	vxcPartnerConfiguration
+	ConnectType      types.String `tfsdk:"connect_type"`
+	VirtualCircuitId types.String `tfsdk:"virtual_circuit_id"`
+}
+
+// vxcEndConfigurationModel maps the end configuration schema data.
+type vxcEndConfigurationModel struct {
+	OwnerUID              types.String `tfsdk:"owner_uid"`
+	UID                   types.String `tfsdk:"product_uid"`
+	Name                  types.String `tfsdk:"product_name"`
+	LocationID            types.Int64  `tfsdk:"location_id"`
+	Location              types.String `tfsdk:"location"`
+	VLAN                  int          `tfsdk:"vlan"`
+	InnerVLAN             types.Int64  `tfsdk:"inner_vlan"`
+	NetworkInterfaceIndex types.Int64  `tfsdk:"vnic_index"`
+	SecondaryName         types.String `tfsdk:"secondary_name"`
 }
 
 func (orm *vxcResourceModel) fromAPIVXC(v *megaport.VXC) {
@@ -82,6 +193,71 @@ func (orm *vxcResourceModel) fromAPIVXC(v *megaport.VXC) {
 	orm.ContractStartDate = types.StringValue(v.ContractStartDate.Format(time.RFC850))
 	orm.ContractEndDate = types.StringValue(v.ContractEndDate.Format(time.RFC850))
 
+	orm.AEndConfiguration = &vxcEndConfigurationModel{
+		OwnerUID:              types.StringValue(v.AEndConfiguration.OwnerUID),
+		UID:                   types.StringValue(v.AEndConfiguration.UID),
+		Name:                  types.StringValue(v.AEndConfiguration.Name),
+		LocationID:            types.Int64Value(int64(v.AEndConfiguration.LocationID)),
+		Location:              types.StringValue(v.AEndConfiguration.Location),
+		VLAN:                  v.AEndConfiguration.VLAN,
+		InnerVLAN:             types.Int64Value(int64(v.AEndConfiguration.InnerVLAN)),
+		NetworkInterfaceIndex: types.Int64Value(int64(v.AEndConfiguration.NetworkInterfaceIndex)),
+		SecondaryName:         types.StringValue(v.AEndConfiguration.SecondaryName),
+	}
+
+	orm.BEndConfiguration = &vxcEndConfigurationModel{
+		OwnerUID:              types.StringValue(v.BEndConfiguration.OwnerUID),
+		UID:                   types.StringValue(v.BEndConfiguration.UID),
+		Name:                  types.StringValue(v.BEndConfiguration.Name),
+		LocationID:            types.Int64Value(int64(v.BEndConfiguration.LocationID)),
+		Location:              types.StringValue(v.BEndConfiguration.Location),
+		VLAN:                  v.BEndConfiguration.VLAN,
+		InnerVLAN:             types.Int64Value(int64(v.BEndConfiguration.InnerVLAN)),
+		NetworkInterfaceIndex: types.Int64Value(int64(v.BEndConfiguration.NetworkInterfaceIndex)),
+		SecondaryName:         types.StringValue(v.BEndConfiguration.SecondaryName),
+	}
+
+	orm.VXCApproval = &vxcApprovalModel{
+		Status:   types.StringValue(v.VXCApproval.Status),
+		Message:  types.StringValue(v.VXCApproval.Message),
+		UID:      types.StringValue(v.VXCApproval.UID),
+		Type:     types.StringValue(v.VXCApproval.Type),
+		NewSpeed: types.Int64Value(int64(v.VXCApproval.NewSpeed)),
+	}
+
+	resources := &vxcResourcesModel{
+		VLL: &vllConfigModel{
+			AEndVLAN:      types.Int64Value(int64(v.Resources.VLL.AEndVLAN)),
+			BEndVLAN:      types.Int64Value(int64(v.Resources.VLL.BEndVLAN)),
+			Description:   types.StringValue(v.Resources.VLL.Description),
+			ID:            types.Int64Value(int64(v.Resources.VLL.ID)),
+			Name:          types.StringValue(v.Resources.VLL.Name),
+			RateLimitMBPS: types.Int64Value(int64(v.Resources.VLL.RateLimitMBPS)),
+			ResourceName:  types.StringValue(v.Resources.VLL.ResourceName),
+			ResourceType:  types.StringValue(v.Resources.VLL.ResourceType),
+		},
+		VirtualRouter: &virtualRouterModel{
+			MCRAsn:             types.Int64Value(int64(v.Resources.VirtualRouter.MCRAsn)),
+			ResourceName:       types.StringValue(v.Resources.VirtualRouter.ResourceName),
+			ResourceType:       types.StringValue(v.Resources.VirtualRouter.ResourceType),
+			Speed:              types.Int64Value(int64(v.Resources.VirtualRouter.Speed)),
+			BGPShutdownDefault: types.BoolValue(v.Resources.VirtualRouter.BGPShutdownDefault),
+		},
+	}
+	for _, i := range v.Resources.Interface {
+		resources.Interface = append(resources.Interface, &portInterfaceModel{
+			Demarcation:  types.StringValue(i.Demarcation),
+			Description:  types.StringValue(i.Description),
+			ID:           types.Int64Value(int64(i.ID)),
+			LOATemplate:  types.StringValue(i.LOATemplate),
+			Media:        types.StringValue(i.Media),
+			Name:         types.StringValue(i.Name),
+			PortSpeed:    types.Int64Value(int64(i.PortSpeed)),
+			ResourceName: types.StringValue(i.ResourceName),
+			ResourceType: types.StringValue(i.ResourceType),
+			Up:           types.Int64Value(int64(i.Up)),
+		})
+	}
 	// TODO - Struct Model Fields
 }
 
@@ -159,8 +335,38 @@ func (r *vxcResource) Create(ctx context.Context, req resource.CreateRequest, re
 		return
 	}
 
-	// TODO - Parse Logic From Plan to Buy Request in Go SDK
-	buyReq := &megaport.BuyVXCRequest{}
+	buyReq := &megaport.BuyVXCRequest{
+		PortUID:   plan.PortUID.ValueString(),
+		VXCName:   plan.Name.ValueString(),
+		RateLimit: int(plan.RateLimit.ValueInt64()),
+
+		WaitForProvision: true,
+		WaitForTime:      10 * time.Minute,
+	}
+
+	aEnd := megaport.VXCOrderEndpointConfiguration{
+		ProductUID: plan.AEndOrderConfiguration.ProductUID.ValueString(),
+		VLAN:       int(plan.AEndOrderConfiguration.VLAN.ValueInt64()),
+	}
+	if plan.AEndOrderConfiguration.vxcOrderMVEConfigModel != nil {
+		aEnd.VXCOrderMVEConfig = &megaport.VXCOrderMVEConfig{
+			InnerVLAN:             int(plan.AEndOrderConfiguration.InnerVLAN.ValueInt64()),
+			NetworkInterfaceIndex: int(plan.AEndOrderConfiguration.NetworkInterfaceIndex.ValueInt64()),
+		}
+	}
+	aEnd.PartnerConfig = toAPIPartnerConfig(*plan.AEndOrderConfiguration.PartnerConfig)
+
+	bEnd := megaport.VXCOrderEndpointConfiguration{
+		ProductUID: plan.BEndOrderConfiguration.ProductUID.ValueString(),
+		VLAN:       int(plan.BEndOrderConfiguration.VLAN.ValueInt64()),
+	}
+	if plan.BEndOrderConfiguration.vxcOrderMVEConfigModel != nil {
+		bEnd.VXCOrderMVEConfig = &megaport.VXCOrderMVEConfig{
+			InnerVLAN:             int(plan.BEndOrderConfiguration.InnerVLAN.ValueInt64()),
+			NetworkInterfaceIndex: int(plan.BEndOrderConfiguration.NetworkInterfaceIndex.ValueInt64()),
+		}
+	}
+	bEnd.PartnerConfig = toAPIPartnerConfig(*plan.BEndOrderConfiguration.PartnerConfig)
 
 	createdVXC, err := r.client.VXCService.BuyVXC(ctx, buyReq)
 	if err != nil {
@@ -313,4 +519,38 @@ func (r *vxcResource) Configure(_ context.Context, req resource.ConfigureRequest
 func (r *vxcResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	// Retrieve import ID and save to id attribute
 	resource.ImportStatePassthroughID(ctx, path.Root("uid"), req, resp)
+}
+
+func toAPIPartnerConfig(c vxcPartnerConfiguration) megaport.VXCPartnerConfiguration {
+	switch p := c.(type) {
+	case *vxcPartnerConfigAWSModel:
+		return &megaport.VXCPartnerConfigAWS{
+			ConnectType:       p.ConnectType.ValueString(),
+			Type:              p.Type.ValueString(),
+			OwnerAccount:      p.OwnerAccount.ValueString(),
+			ASN:               int(p.ASN.ValueInt64()),
+			AmazonASN:         int(p.AmazonASN.ValueInt64()),
+			AuthKey:           p.AuthKey.ValueString(),
+			Prefixes:          p.Prefixes.ValueString(),
+			CustomerIPAddress: p.CustomerIPAddress.ValueString(),
+			AmazonIPAddress:   p.AmazonIPAddress.ValueString(),
+			ConnectionName:    p.ConnectionName.ValueString(),
+		}
+	case *vxcPartnerConfigAzureModel:
+		return &megaport.VXCPartnerConfigAzure{
+			ConnectType: p.ConnectType.ValueString(),
+			ServiceKey:  p.ServiceKey.ValueString(),
+		}
+	case *vxcPartnerConfigGoogleModel:
+		return &megaport.VXCPartnerConfigGoogle{
+			ConnectType: p.ConnectType.ValueString(),
+			PairingKey:  p.PairingKey.ValueString(),
+		}
+	case *vxcPartnerConfigOracleModel:
+		return &megaport.VXCPartnerConfigOracle{
+			ConnectType:      p.ConnectType.ValueString(),
+			VirtualCircuitId: p.VirtualCircuitId.ValueString(),
+		}
+	}
+	return nil
 }
