cfbd49f39a38b4cfc53d7e4cf44c453f3c6a2ac6
Alex Godwin <alex.godwin@megaport.com>
2024-06-04 16:53:19 -0400

    fix: removes dead code

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -6,7 +6,6 @@ package provider
 import (
 	"fmt"
 	"os"
-	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -28,10 +27,4 @@ provider "megaport" {
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
 	"megaport":    providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
 }
