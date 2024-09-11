fd8f02c664611406e5794442c287907556fb1af3
MegaportPhilipBrowne <philip.browne@megaport.com>
2024-09-11 12:23:28 -0400

    fix: add warning for unknown csp connection type instead of erroring out

--- internal/provider/vxc_resource.go
+++ internal/provider/vxc_resource.go
@@ -2374,7 +2374,7 @@ func (r *vxcResource) Create(ctx context.Context, req resource.CreateRequest, re
 				}
 				if !iface.IPRoutes.IsNull() {
 					ipRoutes := []*ipRouteModel{}
-					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, ipRoutes, false)
+					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, &ipRoutes, true)
 					resp.Diagnostics = append(resp.Diagnostics, ipRouteDiags...)
 					for _, ipRoute := range ipRoutes {
 						toAppend.IpRoutes = append(toAppend.IpRoutes, megaport.IpRoute{
@@ -2513,7 +2513,7 @@ func (r *vxcResource) Create(ctx context.Context, req resource.CreateRequest, re
 
 			aEndMegaportConfig := megaport.VXCOrderVrouterPartnerConfig{}
 			ifaceModels := []*vxcPartnerConfigInterfaceModel{}
-			ifaceDiags := partnerConfigAEnd.Interfaces.ElementsAs(ctx, &ifaceModels, false)
+			ifaceDiags := partnerConfigAEnd.Interfaces.ElementsAs(ctx, &ifaceModels, true)
 			resp.Diagnostics = append(resp.Diagnostics, ifaceDiags...)
 			for _, iface := range ifaceModels {
 				toAppend := megaport.PartnerConfigInterface{}
@@ -2525,7 +2525,7 @@ func (r *vxcResource) Create(ctx context.Context, req resource.CreateRequest, re
 				}
 				if !iface.IPRoutes.IsNull() {
 					ipRoutes := []*ipRouteModel{}
-					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, ipRoutes, false)
+					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, &ipRoutes, true)
 					resp.Diagnostics = append(resp.Diagnostics, ipRouteDiags...)
 					for _, ipRoute := range ipRoutes {
 						toAppend.IpRoutes = append(toAppend.IpRoutes, megaport.IpRoute{
@@ -3017,7 +3017,7 @@ func (r *vxcResource) Create(ctx context.Context, req resource.CreateRequest, re
 				}
 				if !iface.IPRoutes.IsNull() {
 					ipRoutes := []*ipRouteModel{}
-					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, ipRoutes, false)
+					ipRouteDiags := iface.IPRoutes.ElementsAs(ctx, &ipRoutes, true)
 					resp.Diagnostics = append(resp.Diagnostics, ipRouteDiags...)
 					for _, ipRoute := range ipRoutes {
 						toAppend.IpRoutes = append(toAppend.IpRoutes, megaport.IpRoute{
@@ -3527,7 +3527,13 @@ func fromAPICSPConnection(ctx context.Context, c megaport.CSPConnectionConfig) (
 		transitObject, transitObjectDiags := types.ObjectValueFrom(ctx, cspConnectionFullAttrs, transitModel)
 		apiDiags = append(apiDiags, transitObjectDiags...)
 		return transitObject, apiDiags
+	default:
+		cspConnectionModel := &cspConnectionModel{
+			ConnectType: types.StringValue("UNKNOWN"),
+		}
+		cspConnectionObject, cspConnectionDiags := types.ObjectValueFrom(ctx, cspConnectionFullAttrs, cspConnectionModel)
+		apiDiags.AddWarning("Unknown CSP Connection type", fmt.Sprintf("Unknown CSP Connection type: %v", provider))
+		apiDiags = append(apiDiags, cspConnectionDiags...)
+		return cspConnectionObject, apiDiags
 	}
-	apiDiags.AddError("Error creating CSP Connection", "Could not create CSP Connection, unknown type")
-	return types.ObjectNull(cspConnectionFullAttrs), apiDiags
 }
