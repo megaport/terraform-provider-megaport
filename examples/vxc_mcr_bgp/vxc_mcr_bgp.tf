71bd68af4b20cfdd5c2ee6108794ba183c3243c4
MegaportPhilipBrowne <philip.browne@megaport.com>
2025-12-01 15:17:59 -0800

    docs: remove embedded prefix_filter_lists from MCR examples to reflect standalone resource

--- examples/vxc_mcr_bgp/vxc_mcr_bgp.tf
+++ examples/vxc_mcr_bgp/vxc_mcr_bgp.tf
@@ -27,25 +27,27 @@ resource "megaport_mcr" "mcr" {
   port_speed           = 5000
   asn                  = 64555
   cost_centre          = "MCR Example"
+}
 
-  prefix_filter_lists = [{
-    description    = "Megaport Example Prefix Filter List"
-    address_family = "IPv4"
-    entries = [
-      {
-        action = "permit"
-        prefix = "10.0.1.0/24"
-        ge     = 24
-        le     = 24
-      },
-      {
-        action = "deny"
-        prefix = "10.0.2.0/24"
-        ge     = 24
-        le     = 24
-      }
-    ]
-  }]
+resource "megaport_mcr_prefix_filter_list" "example" {
+  mcr_id         = megaport_mcr.mcr.product_uid
+  description    = "Megaport Example Prefix Filter List"
+  address_family = "IPv4"
+
+  entries = [
+    {
+      action = "permit"
+      prefix = "10.0.1.0/24"
+      ge     = 24
+      le     = 24
+    },
+    {
+      action = "deny"
+      prefix = "10.0.2.0/24"
+      ge     = 24
+      le     = 24
+    }
+  ]
 }
 
 resource "megaport_vxc" "aws_vxc" {
@@ -83,7 +85,7 @@ resource "megaport_vxc" "aws_vxc" {
               bfd_enabled      = true
               export_policy    = "deny"
               permit_export_to = ["10.0.1.2"]
-              import_whitelist = "Megaport Example Prefix Filter List"
+              import_whitelist = megaport_mcr_prefix_filter_list.example.description
             }
           ]
         }
